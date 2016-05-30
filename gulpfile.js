12fa123634b33aa43865cd1e9d995cb3fa2bd1b5
Toshiaki.Tanaka <Toshiaki.Tanaka@jp.sony.com>
2016-05-30 20:43:42 +0900

    Update. add flash plugin.

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp = require('gulp');
+var del = require('del');
+var ts = require('gulp-typescript');
+var packager = require('electron-packager');
+
+var project = ts.createProject('src/tsconfig.json', {
+    typescript: require('typescript')
+});
+
+gulp.task( 'build:izarra', function () {
+    var result = project.src()
+        .pipe(ts(project));
+    return result.pipe(gulp.dest('build'))
+});
+
+gulp.task( 'clean:izarra', function () {
+    del([
+        'build',
+        'release'
+    ]);
+});
+
+gulp.task( 'copy:izarra', function() {
+    gulp.src(
+        ['src/html/*.html', 'src/html/css/**', 'src/html/js/*.js'],
+        { base: 'src/html' }
+    ).pipe( gulp.dest( 'build/html' ));
+    gulp.src(
+        [ 'plugin/**' ], { base: 'plugin' }
+    ).pipe( gulp.dest( 'build/plugin' ) );
+    gulp.src(
+        [ 'package.json' ], { base: '.' }
+    ).pipe( gulp.dest( 'build' ) );
+});
+
+gulp.task('package:win32:izarra', ["setup:izarra"], function (done) {
+    packager({
+        dir: 'build',
+        out: 'release/win32',
+        name: 'Iwaf',
+        arch: 'ia32',
+        platform: 'win32',
+        version: '0.37.8'
+    }, function (err, path) {
+        done();
+    });
+});
+
+gulp.task("setup:izarra", ["build:izarra", "copy:izarra"]);
+gulp.task("default", ["clean:izarra", "setup:izarra", "package:win32:izarra"]);
